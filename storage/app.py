9b926a85e93901a32666d73b7d62bbffe30467c7
jaspreet0708 <64561183+jaspreet0708@users.noreply.github.com>
2022-04-08 06:05:51 -0700

    setting up lab 10

--- storage/app.py
+++ storage/app.py
@@ -134,7 +134,7 @@ def process_messages():
     client = KafkaClient(hosts=hostname)
     topic = client.topics[str.encode(app_config["events"]["topic"])]
 
-    consumer = topic.get_simple_consumer(consumer_group=b'events_group', reset_offset_on_start=False, auto_offset_reset=OffsetType.LATEST)
+    consumer = topic.get_simple_consumer(consumer_group=b'bruh_group', reset_offset_on_start=False, auto_offset_reset=OffsetType.LATEST)
 
     for msg in consumer:
         msg_str = msg.value.decode('utf-8')
