42c5b3cdc09e6b18b64a04f925004eb13cc6dc9f
jaspreet0708 <64561183+jaspreet0708@users.noreply.github.com>
2022-04-07 19:13:12 -0700

    i see

--- processing/app.py
+++ processing/app.py
@@ -119,10 +119,11 @@ def get_stats():
 
     return stats, 200
 
-app = connexion.FlaskApp(__name__, specification_dir='', strict_validation=True, validate_responses=True)
+app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yaml")
-
+# , strict_validation=True, validate_responses=True
 
 if __name__ == "__main__": 
-    app.run(host='0.0.0.0',port=8100, use_reloader=False)
     init_scheduler() 
+    app.run(host='0.0.0.0',port=8100, use_reloader=False)
+   
