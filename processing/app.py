1b1ecd894c3ec5c02ecee16af489b937ebb8b475
jaspreet0708 <64561183+jaspreet0708@users.noreply.github.com>
2022-04-07 19:08:37 -0700

    wtf is happening

--- processing/app.py
+++ processing/app.py
@@ -124,6 +124,5 @@ app.add_api("openapi.yaml")
 # , strict_validation=True, validate_responses=True
 
 if __name__ == "__main__": 
-    
     app.run(host='0.0.0.0',port=8100, use_reloader=False)
     init_scheduler() 
